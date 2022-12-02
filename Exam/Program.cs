2c78c83a8e33ca21c2f4bb93cf0ce4aab52ba32c
rzdman72 <max001@rambler.ru>
2022-12-02 18:58:29 +0500

    Добавьте файлы проекта.

---
+++ Exam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Мысли вслух
+            //Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+            //
+            //dic.Add("aa", new List<string>(){ "aaa", "bbbb" });
+            //
+            //List<string> strings = new List<string>();
+            //dic.TryGetValue("aa", out strings);
+            //
+            //strings.ForEach(s => Console.WriteLine(s));
+            //strings.RemoveAt(1);
+            //
+            //
+            //strings.ForEach(s => Console.WriteLine(s));
+            //
+            //foreach (var s in strings)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //
+            //dic.Remove("aa");
+            //dic.Add("aa", strings);;
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exam/WordClasses.cs
@@ -0,0 +1,60 @@
+﻿using Exam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+-WordRus(WordEng)
+--Хранит русское(английское) слово
+--единственное поле - string, в котором точно лежит русское (английское) слово
+--проверка на этапе конструктора
+---если все символы русские, значит русское (если все английские - английское)
+*/
+
+namespace Exam
+{
+    internal class WordRus
+    {
+        private string word { get; set; }
+
+        WordRus(string inputword)
+        {
+            //if inputword is rus
+            word = inputword;
+            //else throw a fucking exception i guess
+        }
+        void SetWord(string inputword)
+        {
+            //if inputword is rus
+            word = inputword;
+            //else throw a fucking exception i guess
+        }
+        public override string ToString()
+        {
+            return word;
+        }
+    }
+    internal class WordEng
+    {
+        private string word { get; set; }
+
+        WordEng(string inputword)
+        {
+            //if inputword is eng
+            word = inputword;
+            //else throw a fucking exception i guess
+        }
+        void SetWord(string inputword)
+        {
+            //if inputword is eng
+            word = inputword;
+            //else throw a fucking exception i guess
+        }
+        public override string ToString()
+        {
+            return word;
+        }
+    }
+}
